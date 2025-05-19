7eea0fc5cf68f9e726a7913b0ad94688bdbbdb6b
DannyLavelle <dannyjacklavelle@gmail.com>
2025-05-19 21:09:22 +0100

    Attempt 1
    attempt 1 of generating dungeon - no corridors

--- Assets/Script/DungeonGenerator.cs
+++ Assets/Script/DungeonGenerator.cs
@@ -5,12 +5,12 @@ public class DungeonGenerator : MonoBehaviour
 
     public GameObject floor;
     public GameObject wall;
-    public float dungeonSizeX;
-    public float dungeonSizeY;
+    public int dungeonSizeX;
+    public int dungeonSizeY;
     public int roomMaxSizeX;
     public int roomMaxSizeY;
-    public float roomMinSizeX;
-    public float roomMinSizeY;
+    public int roomMinSizeX;
+    public int roomMinSizeY;
 
     public enum TileType
     {
@@ -20,15 +20,82 @@ public class DungeonGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GenerateDungeoon();
     }
 
     public void GenerateDungeoon()
     {
+        TileType[,] grid = GenerateDungeonLayout(dungeonSizeX, dungeonSizeY, roomMaxSizeX, roomMinSizeY, roomMinSizeX, roomMinSizeY);
+        SpawnDungeon(grid);
+    }
+
+
+    public TileType[,] GenerateDungeonLayout(int dungeonSizeX, int dungeonSizeY, int roomMaxSizeX, int roomMaxSizeY, int roomMinSizeX, int roomMinSizeY)
+    {
+        int cellsX = dungeonSizeX / roomMaxSizeX;
+        int cellsY = dungeonSizeY / roomMaxSizeY;
+
+        TileType[,] dungeonGrid = new TileType[dungeonSizeX, dungeonSizeY];
+
+
+        // Initialize everything as walls
+        for (int x = 0; x < dungeonSizeX; x++)
+        {
+            for (int y = 0; y < dungeonSizeY; y++)
+            {
+                dungeonGrid[x, y] = TileType.Wall;
+            }
+        }
+
+
+
+
+        for (int cellX = 0; cellX < cellsX; cellX++)
+        {
+            for (int cellY = 0; cellY < cellsY; cellY++)
+            {
+                if (Random.value <= 0.4f)
+                    continue;
+
+                dungeonGrid = PlaceRoomInCell(dungeonGrid, cellX, cellY);
+            }
+        }
+
+
+
+
+        return dungeonGrid;
 
     }
 
-    
+
+    private TileType[,] PlaceRoomInCell(TileType[,] dungeonGrid, int cellX, int cellY)
+    {
+        TileType[,] room = GenerateRoomTypes(roomMaxSizeX, roomMaxSizeY, roomMinSizeX, roomMinSizeY);
+
+        int offsetX = cellX * roomMaxSizeX;
+        int offsetY = cellY * roomMaxSizeY;
+
+        for (int x = 0; x < roomMaxSizeX; x++)
+        {
+            for (int y = 0; y < roomMaxSizeY; y++)
+            {
+                int globalX = offsetX + x;
+                int globalY = offsetY + y;
+
+                if (globalX < dungeonGrid.GetLength(0) && globalY < dungeonGrid.GetLength(1))
+                {
+                    dungeonGrid[globalX, globalY] = room[x, y];
+                }
+            }
+        }
+
+        return dungeonGrid;
+    }
+
+
+
+
     public TileType[,] GenerateRoomTypes(int maxX, int maxY, int minX, int minY)
     {
 
@@ -67,4 +134,39 @@ public class DungeonGenerator : MonoBehaviour
         return roomGrid;
 
     }
+
+
+
+    public void SpawnDungeon(TileType[,] grid)
+    {
+        int width = grid.GetLength(0);
+        int height = grid.GetLength(1); // This is now your Z-axis in world space
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Vector3 position = new Vector3(x, 0, z); // Y=0 is ground level
+
+                GameObject prefabToSpawn = null;
+
+                switch (grid[x, z])
+                {
+                    case TileType.Floor:
+                    prefabToSpawn = floor;
+                    break;
+                    case TileType.Wall:
+                    prefabToSpawn = wall;
+                    break;
+                }
+
+                if (prefabToSpawn != null)
+                {
+                    Instantiate(prefabToSpawn, position, Quaternion.identity, this.transform);
+                }
+            }
+        }
+    }
+
+
 }
