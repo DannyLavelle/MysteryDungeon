79709c1b9ef2d66f12404cc2ce1b80e54aa4a3b1
DannyLavelle <dannyjacklavelle@gmail.com>
2025-05-19 23:13:03 +0100

    Progress on method 2
    splits dungeon and  resets it

--- Assets/Script/DungeonGenerator.cs
+++ Assets/Script/DungeonGenerator.cs
@@ -1,171 +1,112 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class DungeonGenerator : MonoBehaviour
 {
 
     public GameObject floor;
     public GameObject wall;
-    public int dungeonSizeX;
-    public int dungeonSizeY;
-    public int roomMaxSizeX;
-    public int roomMaxSizeY;
-    public int roomMinSizeX;
-    public int roomMinSizeY;
+    public int dungeonSizeX = 52;
+    public int dungeonSizeY = 28;
+    public int roomNumX = 1;
+    public int roomNumY = 1;
+    private int GenType;
 
+
+    private List<((int x, int y) topLeft, (int x, int y) topRight, (int x, int y) bottomLeft, (int x, int y) bottomRight, (int x, int y) centre)> roomCorners;
+        
     public enum TileType
     {
         Wall,
         Floor
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
-        GenerateDungeoon();
+        roomCorners = new List<((int x, int y) topLeft, (int x, int y) topRight, (int x, int y) bottomLeft, (int x, int y) bottomRight, (int x, int y) centre)>();
+        GenerateDungeon();
     }
 
-    public void GenerateDungeoon()
+    public void GenerateDungeon()
     {
-        TileType[,] grid = GenerateDungeonLayout(dungeonSizeX, dungeonSizeY, roomMaxSizeX, roomMinSizeY, roomMinSizeX, roomMinSizeY);
-        SpawnDungeon(grid);
+        TileType[,] dungeon = new TileType[dungeonSizeX,dungeonSizeY];
+
+        dungeon = ResetDungeon(dungeon);
+
+
+
+        int roomsX = dungeonSizeX / roomNumX;
+        int roomsY = dungeonSizeY / roomNumY;
+
+        roomCorners = SplitDungeon(dungeon,roomNumX,roomNumY);
+
+
+
+        
     }
 
-
-    public TileType[,] GenerateDungeonLayout(int dungeonSizeX, int dungeonSizeY, int roomMaxSizeX, int roomMaxSizeY, int roomMinSizeX, int roomMinSizeY)
+    private List<((int x, int y) topLeft, (int x, int y) topRight, (int x, int y) bottomLeft, (int x, int y) bottomRight, (int x, int y) center)>
+        SplitDungeon(TileType[,] grid, int partsX, int partsY)
     {
-        int cellsX = dungeonSizeX / roomMaxSizeX;
-        int cellsY = dungeonSizeY / roomMaxSizeY;
+        int totalWidth = grid.GetLength(1);
+        int totalHeight = grid.GetLength(0);
 
-        TileType[,] dungeonGrid = new TileType[dungeonSizeX, dungeonSizeY];
+        int partWidth = totalWidth / partsX;
+        int partHeight = totalHeight / partsY;
 
+        var result = new List<((int, int), (int, int), (int, int), (int, int), (int, int))>();
 
-        // Initialize everything as walls
-        for (int x = 0; x < dungeonSizeX; x++)
+        for (int py = 0; py < partsY; py++)
         {
-            for (int y = 0; y < dungeonSizeY; y++)
+            for (int px = 0; px < partsX; px++)
             {
-                dungeonGrid[x, y] = TileType.Wall;
+                int startX = px * partWidth;
+                int startY = py * partHeight;
+
+                int endX = startX + partWidth - 1;
+                int endY = startY + partHeight - 1;
+
+                var topLeft = (startX, startY);
+                var topRight = (endX, startY);
+                var bottomLeft = (startX, endY);
+                var bottomRight = (endX, endY);
+
+                var centre = ((startX + endX) / 2, (startY + endY) / 2);
+
+                result.Add((topLeft, topRight, bottomLeft, bottomRight, centre));
             }
         }
 
-
-
-
-        for (int cellX = 0; cellX < cellsX; cellX++)
-        {
-            for (int cellY = 0; cellY < cellsY; cellY++)
-            {
-                if (Random.value <= 0.4f)
-                    continue;
-
-                dungeonGrid = PlaceRoomInCell(dungeonGrid, cellX, cellY);
-            }
-        }
-
-
-
-
-        return dungeonGrid;
-
+        return result;
     }
 
 
-    private TileType[,] PlaceRoomInCell(TileType[,] dungeonGrid, int cellX, int cellY)
+    private TileType[,] GenerateRoom(TileType[,] tile, int index)
     {
-        TileType[,] room = GenerateRoomTypes(roomMaxSizeX, roomMaxSizeY, roomMinSizeX, roomMinSizeY);
+        
 
-        int offsetX = cellX * roomMaxSizeX;
-        int offsetY = cellY * roomMaxSizeY;
 
-        for (int x = 0; x < roomMaxSizeX; x++)
-        {
-            for (int y = 0; y < roomMaxSizeY; y++)
-            {
-                int globalX = offsetX + x;
-                int globalY = offsetY + y;
-
-                if (globalX < dungeonGrid.GetLength(0) && globalY < dungeonGrid.GetLength(1))
-                {
-                    dungeonGrid[globalX, globalY] = room[x, y];
-                }
-            }
-        }
-
-        return dungeonGrid;
+        return tile;
     }
 
 
 
-
-    public TileType[,] GenerateRoomTypes(int maxX, int maxY, int minX, int minY)
+    private TileType[,] ResetDungeon(TileType[,] tilemap)
     {
-
-        //new grid
-        TileType[,] roomGrid = new TileType[maxX, maxY];
-
-
-
-        for (int x = 0; x < roomMaxSizeX; x++)// fill room with walls 
-        {
-            for (int y = 0; y < roomMaxSizeY; y++)
-            {
-                roomGrid[x, y] = TileType.Wall;
-            }
-        }
-
-        int floorWidth = Random.Range(minX, maxX + 1);
-        int floorHeight = Random.Range(minY, maxY + 1);
-
-
-        // Choose a spot where the room will stil fit 
-        int offsetX = Random.Range(0, roomMaxSizeX - floorWidth + 1);
-        int offsetY = Random.Range(0, roomMaxSizeY - floorHeight + 1);
-
-
-
-        //Add floors
-        for (int x = 0; x < floorWidth; x++)
-        {
-            for (int y = 0; y < floorHeight; y++)
-            {
-                roomGrid[x + offsetX, y + offsetY] = TileType.Floor;
-            }
-        }
-
-        return roomGrid;
-
-    }
-
-
-
-    public void SpawnDungeon(TileType[,] grid)
-    {
-        int width = grid.GetLength(0);
-        int height = grid.GetLength(1); // This is now your Z-axis in world space
+        int width = tilemap.GetLength(0);
+        int height = tilemap.GetLength(1);
 
         for (int x = 0; x < width; x++)
         {
-            for (int z = 0; z < height; z++)
+            for (int y = 0; y < height; y++)
             {
-                Vector3 position = new Vector3(x, 0, z); // Y=0 is ground level
-
-                GameObject prefabToSpawn = null;
-
-                switch (grid[x, z])
-                {
-                    case TileType.Floor:
-                    prefabToSpawn = floor;
-                    break;
-                    case TileType.Wall:
-                    prefabToSpawn = wall;
-                    break;
-                }
-
-                if (prefabToSpawn != null)
-                {
-                    Instantiate(prefabToSpawn, position, Quaternion.identity, this.transform);
-                }
+                tilemap[x, y] = TileType.Wall;
             }
         }
+        return tilemap;
+        
     }
 
 
